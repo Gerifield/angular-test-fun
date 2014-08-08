1ad81d90e6043fc804e9f7f221e759ca3ca372be
Gerifield <gergo254@gmail.com>
2014-08-08 19:53:38 +0200

    Added orderBy

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var app = angular.module('angularTestApp', []);
 
 app.controller('TestRepeatCtrl', function ($scope) {
 	$scope.items = [
-		{'name' : 'Item 1', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel blandit quam, faucibus luctus turpis. Integer ullamcorper a metus non tempus. Mauris ultricies mi a eros vestibulum laoreet.'},
-		{'name' : 'Item 2', 'description': 'Random text'},
-		{'name' : 'Item 3', 'description': 'Lorem 2 stuff'}
+		{'id': 1, 'name' : 'Item 1', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel blandit quam, faucibus luctus turpis. Integer ullamcorper a metus non tempus. Mauris ultricies mi a eros vestibulum laoreet.'},
+		{'id': 3, 'name' : 'Item 2', 'description': 'Random text'},
+		{'id': 2, 'name' : 'Item 3', 'description': 'Lorem 2 stuff'}
 	];
 });
