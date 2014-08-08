4e8802f655bd147747aece5796b8d28107941147
Gerifield <gergo254@gmail.com>
2014-08-08 22:57:25 +0200

    Test directive

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.controller('TestRepeatCtrl', function ($scope) {
 		{'id': 3, 'name' : 'Item 2', 'description': 'Random text'},
 		{'id': 2, 'name' : 'Item 3', 'description': 'Lorem 2 stuff'}
 	];
+
+	$scope.reverseOrder = false;
+});
+
+app.directive('myDir', function(){
+	return {
+		restrict: 'E',
+		template: '<b>{{item.name}}</b><br>{{item.description}}'
+	};
 });
